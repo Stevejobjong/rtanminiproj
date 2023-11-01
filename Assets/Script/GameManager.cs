9a654259a1f6101917407ad2cd6cef9c76c44eb9
Lawrence1031h <lawrence1031h@gmail.com>
2023-11-01 16:24:41 +0900

    Revert "20231101_Finish"
    This reverts commit d9e3242980be909dcfac33e0188b8dddeb6b9fe7.

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -14,10 +14,8 @@ public class GameManager : MonoBehaviour
     //public static event CloseFunction CardClose;
 
     public Text TimeText;
-    public Text bestS;
-    public Text bestSTitle;
-    public Text endText;
-    public GameObject Finish;
+    public Text BestScoreText;
+    public GameObject EndText;
 
     public Card Card;
     public GameObject FirstCard;
@@ -120,15 +118,6 @@ public class GameManager : MonoBehaviour
 
     void GameOver()
     {
-        if (time < 60.0f)
-        {
-            endText.text = "Clear!";
-        }
-        else
-        {
-            endText.text = "Game Over!";
-        }
-
         Time.timeScale = 0.0f;
         if (PlayerPrefs.HasKey("BestScore") == false)
         {
@@ -142,7 +131,7 @@ public class GameManager : MonoBehaviour
             }
         }
         float BestScore = PlayerPrefs.GetFloat("BestScore");
-        bestS.text = BestScore.ToString("N2");
-        Finish.SetActive(true);
+        EndText.SetActive(true);
+        BestScoreText.text = BestScore.ToString("N2");
     }
 }
