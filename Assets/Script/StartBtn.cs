c3c9db274f3d9f4d43a37facc9a69a4f682b6149
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-11-02 10:43:28 +0900

    Update StartBtn.cs

--- Assets/Script/StartBtn.cs
+++ Assets/Script/StartBtn.cs
@@ -15,6 +15,7 @@ public class StartBtn : MonoBehaviour
         audioSource.PlayOneShot(startBtn);
         //버튼 클릭 시 buttonpress로 이미지 변경
         GetComponent<Image>().sprite = Resources.Load<Sprite>("buttonpress");
+        transform.position = new Vector3(transform.position.x, transform.position.y - 27.0f, 0);//버튼 눌릴때 살짝 아래로
         //씬이 곧바로 로드되어 효과음이 재생되지 않는 문제 해결(1초 딜레이)
         Invoke("LoadGameScene", 1f);
     }
