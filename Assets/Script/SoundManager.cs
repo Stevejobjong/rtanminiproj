e3c1b4db8c8ecd4bae207620b3c683e7d130d775
min-vely <jeongmin322@naver.com>
2023-11-02 13:44:37 +0900

    Add SoundManager

---
+++ Assets/Script/SoundManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource bgSound;
+    public AudioClip[] bglist;
+
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for (int i = 0; i < bglist.Length; i++)
+        {
+            //씬 이름과 동일한 브금 재생
+            if (arg0.name == bglist[i].name)
+            {
+                BgSoundPlay(bglist[i]);
+            }
+        }
+        
+
+    }
+
+    public void SFXPlay(string sfxName, AudioClip clip)
+    {
+        GameObject go = new GameObject(sfxName);
+        AudioSource audiosource = go.AddComponent<AudioSource>();
+        audiosource.clip = clip;
+        audiosource.Play();
+
+        //효과음 재생이 끝나면 생성했던 오브젝트 파괴
+        Destroy(go, clip.length);
+    }
+
+    public void BgSoundPlay(AudioClip clip)
+    {
+        bgSound.clip = clip;
+        bgSound.loop = true;
+        bgSound.volume = 0.8f;
+        bgSound.Play();
+    }
+}
