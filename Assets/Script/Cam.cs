8602161dae51642cf22ae2d9da04ade07e09e89b
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2023-11-07 15:41:23 +0900

    fix

--- Assets/Script/Cam.cs
+++ Assets/Script/Cam.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class Cam : MonoBehaviour {
     public static Cam instance;
 
-    private void Awake() {
-        if (instance == null) {
-            instance = this;
-            DontDestroyOnLoad(instance);
-        } else {
-            Destroy(gameObject);
-        }
-    }
     void Start() {
         Screen.SetResolution(760, 1280, false);
     }
